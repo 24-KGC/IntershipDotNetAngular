d7d3fcf3731f89330e383195151837206cbe0dea
Khiem <24560010@gm.uit.edu.vn>
2026-04-06 11:49:52 +0700

    Day 3: Data binding

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HelloComponent } from './hello.component';
+import { FormsModule, NgModel } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloComponent, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
+
 export class App {
   protected readonly title = signal('InternAngular');
-  // abc
 }
